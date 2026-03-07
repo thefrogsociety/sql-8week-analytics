6bacde095c9d7297d479e51e26745ebb98d87715
duy lê <duy.leduc.work@gmail.com>
2026-03-07 09:46:40 +0700

    Add SQL queries for customer purchase analysis
    Added SQL queries to retrieve the first purchased item by each customer, the most purchased item overall, and the most popular item for each customer.

--- Case Study #1 - Danny's Diner/queries.sql
+++ Case Study #1 - Danny's Diner/queries.sql
@@ -21,14 +21,43 @@ GROUP BY customer_id;
 -- What was the first item from the menu purchased by each customer?
 
 SELECT
+  sales.customer_id,
+  sales.order_date AS first_day,
+  menu.product_name
+FROM sales 
+JOIN menu 
+  ON sales.product_id = menu.product_id
+WHERE sales.order_date = (
+  SELECT MIN(order_date)
+  FROM sales
+  WHERE customer_id = sales.customer_id
+);
+
+-- Question 4
+-- What is the most purchased item on the menu and how many times was it purchased by all customers?
+
+SELECT
+  menu.product_name AS most_purchased_product,
+  COUNT(sales.product_id) AS times_purchased
+FROM sales
+JOIN menu
+  ON sales.product_id = menu.product_id
+GROUP BY menu.product_name
+LIMIT 1;
+
+-- Question 5
+-- Which item was the most popular for each customer?
+
+SELECT
   s.customer_id,
-  s.order_date AS first_day,
-  m.product_name
+  m.product_name,
+  COUNT(*) AS purchases,
+  ROW_NUMBER() OVER (
+      PARTITION BY s.customer_id
+      ORDER BY COUNT(*) DESC
+  ) AS rank
 FROM sales s
 JOIN menu m
   ON s.product_id = m.product_id
-WHERE s.order_date = (
-  SELECT MIN(order_date)
-  FROM sales
-  WHERE customer_id = s.customer_id
-);
+GROUP BY s.customer_id, m.product_name;
+
