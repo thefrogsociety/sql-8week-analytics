152a6413537aabdd138aecf2eda82a762fd82b2e
duy lê <duy.leduc.work@gmail.com>
2026-03-21 10:38:08 +0700

    Update schema.sql

--- Case Study #6 - Clique Bait/schema.sql
+++ Case Study #6 - Clique Bait/schema.sql
@@ -34591,3 +34591,40 @@ VALUES
   ('355a6a', '87a4ba', '11', '2', '17', '2020-03-18 22:45:12.670472'),
   ('355a6a', '87a4ba', '12', '1', '18', '2020-03-18 22:45:54.081818'),
   ('355a6a', '87a4ba', '13', '3', '19', '2020-03-18 22:45:54.984666');
+
+-- Enterprise Relationship Diagram
+
+CREATE TABLE clique_bait.event_identifier (
+  "event_type" INTEGER,
+  "event_name" VARCHAR(13)
+);
+
+CREATE TABLE clique_bait.campaign_identifier (
+  "campaign_id" INTEGER,
+  "products" VARCHAR(3),
+  "campaign_name" VARCHAR(33),
+  "start_date" TIMESTAMP,
+  "end_date" TIMESTAMP
+);
+
+CREATE TABLE clique_bait.page_hierarchy (
+  "page_id" INTEGER,
+  "page_name" VARCHAR(14),
+  "product_category" VARCHAR(9),
+  "product_id" INTEGER
+);
+
+CREATE TABLE clique_bait.users (
+  "user_id" INTEGER,
+  "cookie_id" VARCHAR(6),
+  "start_date" TIMESTAMP
+);
+
+CREATE TABLE clique_bait.events (
+  "visit_id" VARCHAR(6),
+  "cookie_id" VARCHAR(6),
+  "page_id" INTEGER,
+  "event_type" INTEGER,
+  "sequence_number" INTEGER,
+  "event_time" TIMESTAMP
+);
